6e75cfa8fd1fcb191d1aa9fc062f07a0c5aaab10
Phuc Ly <lytienphuc0810@gmail.com>
2024-01-26 02:06:58 +0700

    unit test

--- spec/web/controllers/hotels_spec.rb
+++ spec/web/controllers/hotels_spec.rb
@@ -2,10 +2,10 @@ require_relative '../../../apps/web/controllers/hotels/index'
 
 RSpec.describe Web::Controllers::Hotels::Index do
   let(:action) { Web::Controllers::Hotels::Index.new }
-  let(:params) { Hash[] }
+  let(:empty_params) { Hash[] }
 
-  it "is successful" do
+  it "returns 400 status in case of empty params" do
     response = action.call(params)
-    expect(response[0]).to be(200)
+    expect(response[0]).to be(400)
   end
 end
